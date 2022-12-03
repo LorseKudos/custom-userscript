de123a3eaddb6e642c368d317f901d6135341361
Lorse <36875372+LorseKudos@users.noreply.github.com>
2022-12-03 21:17:18 +0900

    Update to v0.2

--- store_list_sort_plugin/main.user.js
+++ store_list_sort_plugin/main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         SortFavoriteStoresInYahooShopping
 // @namespace    https://github.com/LorseKudos
-// @version      0.1
-// @description  ヤフーショッピングのお気に入りページにあるストア一覧をソートします
+// @version      0.2
+// @description  ヤフーショッピングのお気に入りページにあるストア一覧をソートします。ついでに値段変化のテキストを青色にします。
 // @author       Lorse
 // @updateURL    https://github.com/LorseKudos/custom_userscript/raw/main/store_list_sort_plugin/main.user.js
 // @downloadURL  https://github.com/LorseKudos/custom_userscript/raw/main/store_list_sort_plugin/main.user.js
@@ -13,25 +13,27 @@
 
 (function() {
     'use strict';
+    const discountTextClassPrefix = "style_ItemList__itemDiscount__"
+    $(`<style id='css_to_change_discount_text_color' type='text/css'> [class^="${discountTextClassPrefix}"]{ color:blue !important;} </style>`).appendTo("head");
 
+    const storeClassPrefix = "style_Filter__seller__"
+    const storeElSelector = `li[class^="${storeClassPrefix}"]`
     function waitUntilDisplayStores() {
-        if($(".elStoreTitle").length !== 0){
+        if($(storeElSelector).length > 0){
             return;
         }
-        window.setTimeout(() => waitUntilDisplayStores(), 500);
+        window.setTimeout(() => waitUntilDisplayStores(), 100);
     }
     waitUntilDisplayStores();
 
-    let storeNameToElement = $(".elStoreTitle").toArray().reduce((dic,el) => {
+    let storeNameToElement = $(storeElSelector).toArray().reduce((dic,el) => {
         dic[el.innerText] = el;
         return dic;
     }, {});
-    let sortedStores = $(".elStoreTitle").toArray().map((el) => el.innerText).sort().reverse();
+    let sortedStores = Object.keys(storeNameToElement).sort().reverse();
 
     for(let store of sortedStores){
         let elementToInsert = storeNameToElement[store];
-        elementToInsert.parentElement.insertBefore(elementToInsert,$(".elStoreTitle")[0]);
+        elementToInsert.parentElement.insertBefore(elementToInsert,$(storeElSelector)[0]);
     }
-
-    $(".elFilterListTitle").addClass("elShow");
 })();
